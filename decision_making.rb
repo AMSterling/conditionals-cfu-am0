58ad5c6318bb8dfcf078c003c79a54dde1c3a6d5
Anna Marie Sterling <sterling.am@gmail.com>
2022-04-20 21:46:27 -0600

    Add Bear response

--- decision_making.rb
+++ decision_making.rb
@@ -35,9 +35,16 @@ end
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+## Using an if statement that will determine whether the bear will have a hat if door 1 is chosen or a scarf if door 2 (anything other than door 1)
 # 2. What variable has a new value assigned to it after the first if statement executes?
+## bear_clothing is determined after door_choice returns an integer.
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+## scarf because any integer other than 1 determines the value to be scarf with the use of else
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+## These are the possible responses generated from the bear_choice variable if you choose 1, 2 or 3.
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+## You run into a room full of snakes.
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+## The bear will have a hat you point out to the bear it's too small and you make the bear cry.
 # 7. What is your favorite ending?
+## bear_choice >= 4 because you've just made a new friend.
